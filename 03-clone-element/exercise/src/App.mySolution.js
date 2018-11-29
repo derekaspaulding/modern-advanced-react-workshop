827d22c5401e0918d3b2205a6d70d07f1b1a3076
Derek Spaulding <derek.spaulding@code42.com>
2018-11-29 15:57:46 -0600

    finish exercise 3

--- 03-clone-element/exercise/src/App.mySolution.js
+++ 03-clone-element/exercise/src/App.mySolution.js
@@ -5,11 +5,26 @@ import FaForward from "react-icons/lib/fa/forward";
 import FaBackward from "react-icons/lib/fa/backward";
 
 class RadioGroup extends Component {
+  state = {
+    activeValue: this.props.defaultActiveValue
+  };
+
+  setActiveValue = value => {
+    this.setState({ activeValue: value });
+  };
+
   render() {
+    const clones = React.Children.map(this.props.children, (child, index) => {
+      const isActive = child.props.value === this.state.activeValue;
+      return React.cloneElement(child, {
+        isActive,
+        setActiveValue: this.setActiveValue
+      });
+    });
     return (
       <fieldset className="radio-group">
         <legend>{this.props.legend}</legend>
-        {this.props.children}
+        {clones}
       </fieldset>
     );
   }
@@ -17,9 +32,16 @@ class RadioGroup extends Component {
 
 class RadioButton extends Component {
   render() {
-    const isActive = false; // <-- should come from somewhere
+    const { isActive, setActiveValue } = this.props; // <-- should come from somewhere
     const className = "radio-button " + (isActive ? "active" : "");
-    return <button className={className}>{this.props.children}</button>;
+    return (
+      <button
+        className={className}
+        onClick={() => this.props.setActiveValue(this.props.value)}
+      >
+        {this.props.children}
+      </button>
+    );
   }
 }
 
@@ -27,7 +49,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <RadioGroup legend="Radio Group">
+        <RadioGroup legend="Radio Group" defaultActiveValue={"pause"}>
           <RadioButton value="back">
             <FaBackward />
           </RadioButton>
