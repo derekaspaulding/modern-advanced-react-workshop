d5a4a279de3ef078b030c403f05ce06746b9b54d
Derek Spaulding <derek.spaulding@code42.com>
2018-11-29 12:59:44 -0600

    finish excercise 2

---
+++ 02-hocs-render-props/exercise/src/App.myHoc.js
@@ -0,0 +1,48 @@
+import React from "react";
+import createMediaListener from "./lib/createMediaListener";
+import { Galaxy, Trees, Earth } from "./lib/screens";
+
+const media = createMediaListener({
+  big: "(min-width : 1000px)",
+  tiny: "(max-width: 600px)"
+});
+
+const withMedia = (Component) => {
+  return class extends React.Component {
+    state = {
+      media: media.getState()
+    };
+
+    componentDidMount() {
+      media.listen(media => this.setState({ media }));
+    }
+
+    componentWillUnmount() {
+      media.dispose();
+    }
+
+    render() {
+      return <Component media={this.state.media} />
+    }
+  }
+}
+
+class App extends React.Component {
+  render() {
+    const { media } = this.props;
+
+    return (
+      <div>
+        {media.big ? (
+          <Galaxy key="galaxy" />
+        ) : media.tiny ? (
+          <Trees key="trees" />
+        ) : (
+          <Earth key="earth" />
+        )}
+      </div>
+    );
+  }
+}
+
+export default withMedia(App);
